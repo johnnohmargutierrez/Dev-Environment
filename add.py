31d063b11f14b6edeab7f5868d9f9af39e33d9d1
johnnohmargutierrez <johnnohmar.gutierrez@gmail.com>
2026-03-11 19:14:13 +0800

    Reformat add_two_integers function and add comment

--- add.py
+++ add.py
@@ -1,3 +1,4 @@
+#just added a comment
 def add_two_integers(a, b):
 	add_two = a + b
 	print(add_two)
@@ -7,6 +8,7 @@ num_1 = float(input("First num:"))
 num_2 = float(input("Second num:"))
 			  
 add_two_integers(num_1,num_2)
+
 
 
 

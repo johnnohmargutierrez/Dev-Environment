4bd261cb3ecca1b9ce6df6fb1966edc3bbed7e13
johnnohmargutierrez <johnnohmar.gutierrez@gmail.com>
2026-03-11 13:54:58 +0800

    Fix formatting issues in add.py

--- add.py
+++ add.py
@@ -3,9 +3,10 @@ def add_two_integers(a, b):
     
 print("Enter two numbers:")
 num_1 = input("First num:")
-num_2 = input("Second num:"
+num_2 = input("Second num:")
 			  
 add_two_integers(num_1,num_2)
+
 
 
 

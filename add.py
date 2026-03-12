08cc103c34d9a467c6ed0ae6f50855271b934fd2
johnnohmargutierrez <johnnohmar.gutierrez@gmail.com>
2026-03-12 10:28:06 +0800

    Add subtraction function and fix function calls

--- add.py
+++ add.py
@@ -6,13 +6,19 @@ def add_two_integers(a, b):
 def multi_two_integers(a, b):
 	add_two = a * b
 	print(f"Product of {a} * {b} is {add_two}")
+
+def sub_two_integers(a, b):
+	add_two = a - b
+	print(f"Difference of {a} * {b} is {add_two}")
     
 print("Enter two numbers:")
 num_1 = float(input("First num:"))
 num_2 = float(input("Second num:"))
 			  
 add_two_integers(num_1,num_2)
+diff_two_integers(num_1,num_2)
 multi_two_integers(num_1,num_2)
+
 
 
 

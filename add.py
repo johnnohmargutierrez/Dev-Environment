aae0f2eeb993b67ef22757a03db8a67bbfb7d9d5
johnnohmargutierrez <johnnohmar.gutierrez@gmail.com>
2026-03-12 10:32:02 +0800

    Reformat add.py with consistent indentation

--- add.py
+++ add.py
@@ -9,7 +9,7 @@ def multi_two_integers(a, b):
 
 def sub_two_integers(a, b):
 	sub_two = a - b
-	print(f"Difference of {a} * {b} is {add_two}")
+	print(f"Difference of {a} * {b} is {sub_two}")
     
 print("Enter two numbers:")
 num_1 = float(input("First num:"))
@@ -18,6 +18,7 @@ num_2 = float(input("Second num:"))
 add_two_integers(num_1,num_2)
 sub_two_integers(num_1,num_2)
 multi_two_integers(num_1,num_2)
+
 
 
 

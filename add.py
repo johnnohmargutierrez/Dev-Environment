8cba4244ae6788efb738ffe07416cd6dd990d699
johnnohmargutierrez <johnnohmar.gutierrez@gmail.com>
2026-03-12 08:54:42 +0800

    Refactor add_two_integers and multi_two_integers functions

--- add.py
+++ add.py
@@ -1,11 +1,11 @@
 #just added a comment
 def add_two_integers(a, b):
 	add_two = a + b
-	print("Sum of" a "+" b "is" add_two)
+	print(f"Sum of {a} + {b} is {add_two}")
 
 def multi_two_integers(a, b):
 	add_two = a * b
-	print("Product of" a "*" b "is" add_two)
+	print(f"Product of {a} * {b} is {add_two}")
     
 print("Enter two numbers:")
 num_1 = float(input("First num:"))
@@ -13,6 +13,7 @@ num_2 = float(input("Second num:"))
 			  
 add_two_integers(num_1,num_2)
 add_two_integers(num_1,num_2)
+
 
 
 
